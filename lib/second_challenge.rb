c4d70c96b9b3d83b23b0a8d6d90c74624c5b13f7
Hussain Ahmed <haz6798@icloud.com>
2020-02-23 02:51:28 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,11 @@
 def second_challenge
   epic_tragedy = {
-     :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
+    :montague => {
+      #your key/value pairs here!
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
       }}
+
   
 end
